554cba29920375d49d6c3d81eb07fc25d78d6d24
mehmedayt <68188839+mehmedayt@users.noreply.github.com>
2023-08-24 17:01:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); 
     if(!cityName) return;
 
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+    const GEOCODING_API_URL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         if(!data.length) return alert(`No coordinates found for${cityName}`);
@@ -82,7 +82,7 @@ const getUserCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
         position => {
             const {latitude, longitude} = position.coords;
-            const REVERSE_GEOCODING_URL = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`
+            const REVERSE_GEOCODING_URL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`
             
             fetch(REVERSE_GEOCODING_URL).then(res => res.json()).then(data => {
                 const {name} = data[0];
