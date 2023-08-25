23f7d32165bff9c466a5ec4dd1572f1a084947f1
mehmedayt <68188839+mehmedayt@users.noreply.github.com>
2023-08-25 22:59:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const getCityCoordinates = () => {
     const cityName = cityInput.value.trim(); 
     if(!cityName) return;
 
-    const GEOCODING_API_URL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+    const GEOCODING_API_URL = `${proxyUrl}http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         if(!data.length) return alert(`No coordinates found for${cityName}`);
